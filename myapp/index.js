d34d0d90d7c39665b116c4aabea45b2cb365736a
FavioPaz <familcard.13@Gmail.com>
2023-03-18 14:38:45 -0600

    hola

---
+++ myapp/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
+
+  "devDependencies"; {
+    "eslint"; "^4.12.1"
+  }
+
+ 
